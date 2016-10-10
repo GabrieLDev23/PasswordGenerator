513883e79c44811a4ab09e1ce16f7f834a9b0d0d
GabrieLDev <gabr.cappellaro@gmail.com>
2016-10-10 20:12:01 +0200

    Create BottomSheet

--- app/src/main/java/io/github/gabrieldev/passwordgenerator/MainActivity.java
+++ app/src/main/java/io/github/gabrieldev/passwordgenerator/MainActivity.java
@@ -1,18 +1,23 @@
 package io.github.gabrieldev.passwordgenerator;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.BottomSheetDialog;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,7 +27,10 @@ public class MainActivity extends AppCompatActivity {
     private FloatingActionButton mFabHome;
     private BottomSheetDialog mBottomSheet;
     private Animation mRotateBackward;
-    private String mPasswordGenerate;
+    private String mPasswordGenerate, mTextOfEditText;
+    private LinearLayout mGenerateBottomSheet, mCopyBottomSheet, mDeleteBottomSheet;
+    private ClipboardManager mClipboardManager;
+    private ClipData mClipData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,10 +38,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        mClipboardManager = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
         mButtonGenerate = (Button) findViewById(R.id.mButtonGenerate);
         mTextViewMoreChoose = (TextView) findViewById(R.id.mTextViewMoreChoose);
         mEditTextPassword = (AutoCompleteTextView) findViewById(R.id.mEditTextPassword);
         mFabHome = (FloatingActionButton) findViewById(R.id.mFabHome);
+        mGenerateBottomSheet = (LinearLayout) findViewById(R.id.mGenerateButtonBottomSheet);
+        mCopyBottomSheet = (LinearLayout) findViewById(R.id.mCopyButtonBottomSheet);
+        mDeleteBottomSheet = (LinearLayout) findViewById(R.id.mDeleteButtonBottomSheet);
 
         mRotateBackward = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.rotate_backward);
 
@@ -44,6 +56,32 @@ public class MainActivity extends AppCompatActivity {
         ArrayAdapter<String> mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, new String[] {mPasswordGenerate});
         mEditTextPassword.setAdapter(mAdapter);
 
+        mGenerateBottomSheet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mBottomSheet.dismiss();
+            }
+        });
+        mCopyBottomSheet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mTextOfEditText = mEditTextPassword.getText().toString().trim();
+                if (TextUtils.isEmpty(mTextOfEditText)) {
+                    Toast.makeText(MainActivity.this, R.string.mStringPasswordEmpty, Toast.LENGTH_SHORT).show();
+                } else {
+                    mClipData.newPlainText("",mTextOfEditText);
+                    mClipboardManager.setPrimaryClip(mClipData);
+                }
+                mBottomSheet.dismiss();
+            }
+        });
+        mDeleteBottomSheet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mEditTextPassword.setText("");
+                mBottomSheet.dismiss();
+            }
+        });
         mFabHome.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
